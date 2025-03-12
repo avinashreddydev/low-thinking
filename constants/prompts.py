1fa2735b3e95bad17cc13f7c5c16cd47419222bc
Avinash Reddy <avinashreddy.idio@gmail.com>
2025-03-12 08:17:51 -0400

    changed to src folder, to folow3 the best practices

---
+++ constants/prompts.py
@@ -0,0 +1,20 @@
+# as Suggested by the Deepseek Huggingface model
+MATH_PROMPT = "\nPlease reason step by step, and put your final answer within \boxed{}"
+
+# Summary Generator Prompt
+THINKING_SUMMARY_PROMPT = """
+You are a helpful assistant that summarizes the thinking process of the given problem, thinking and solution
+
+Problem:
+{problem}
+
+Thinking:
+{thinking}
+
+Solution:
+{solution}
+
+"""
+
+if __name__ == "__main__":
+    pass
